66f99b4bf88eabc41722e49338d1223477593740
Emmanuel Tabares <e.tabares20@gmail.com>
2022-04-15 14:02:37 -0300

    Implementacion de algunas rutas HTTP con decoradores - findAll() findBook()

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,13 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Req } from '@nestjs/common';
 import { BooksService } from './books.service'; //Importación del servicio que proporciona los datos
+import { Request } from 'express';
 
 @Controller('books')
 export class BooksController {
-  constructor(private bookService: BooksService) {} //	Constructor con el servicio inyectado
+  constructor(private booksService: BooksService) {} //	Constructor con el servicio inyectado
 
   @Get() // Decorador para indicar la ruta atendida y el método HTTP
-  findAll() {
-    //	Método asociado a la petición
-    return this.bookService.findAll(); //	Llamada al método del servicio que resuelve la petición
+  findAll(@Req() request: Request) {
+    return this.booksService.findAll(request.query);
+  }
+
+  @Get(':bookId')
+  findBook(@Param('bookId') bookId: string) {
+    return this.booksService.findBook(bookId);
   }
 }
--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -2,7 +2,21 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class BooksService {
-  findAll(): any {
-    return 'findAll funcionando';
+  findAll(params): any {
+    let msg = `findAll funcionando. Parámetros:`;
+
+    if (params.order !== undefined) {
+      msg = msg + ` order: ${params.order}`;
+    }
+
+    if (params.limit !== undefined) {
+      msg = msg + ` limit: ${params.limit}`;
+    }
+
+    return msg;
+  }
+
+  findBook(bookId: string): any {
+    return `findBook funcionando con bookId: ${bookId}`;
   }
 }
